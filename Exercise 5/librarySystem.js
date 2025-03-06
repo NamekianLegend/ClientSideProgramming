e634c08f8e9b2261c2a0fc5bf8a2f5592a59d2e5
Josh Leslie <joshleslie1993@gmail.com>
2025-03-06 14:24:30 -0400

    moved console log to appropriate spot

--- Exercise 5/librarySystem.js
+++ Exercise 5/librarySystem.js
@@ -80,6 +80,9 @@ const library = { // Create a library object
     }
 };
 
+console.log("\nPart 3: Creating a library object and calling functions...");
+console.log("===============================================");
+
 console.log(library.addBook({ title: 'The Hobbit', author: 'J.R.R. Tolkien', available: true })); // Add a book to the library
 console.log(library.addBook({ title: 'The Fellowship of the Ring', author: 'J.R.R. Tolkien', available: false })); // Add a book to the library
 console.log(library.addBook({ title: 'The Two Towers', author: 'J.R.R. Tolkien', available: true })); // Add a book to the library
@@ -87,6 +90,5 @@ console.log(library.addBook({ title: 'The Return of the King', author: 'J.R.R. T
 console.log(library.addBook({ title: 'Project Hail Mary', author: 'Andy Weir', available: true })); // Add a book to the library
 console.log(library.addBook({ title: 'Artemis', author: 'Andy Weir', available: true })); // Add a book to the library
 
-console.log("\nPart 3: Creating a library object and calling functions...");
-console.log("===============================================");
+
 console.log("The following books are available: " + library.listAvailableBooks().join(", ")); // Call listAvailableBooks function
